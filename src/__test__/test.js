620f12f626bc4b3103c32ea9f44aaf634c516131
Adrian Bota <adrianbota@users.noreply.github.com>
2020-09-20 15:03:00 +0100

    simplify usage

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,21 +1,13 @@
-import group from '../index.js';
+import g from '../index.js';
 
 test('Jest Group Snapshots', () => {
-    const g = group();
+    const before = document.querySelector('body').cloneNode(false);
+    before.style.color = 'red';
 
-    const before1 = document.querySelector('body').cloneNode(false);
-    before1.style.color = 'red';
-    g.add(before1, 'Body element before');
+    const after = document.querySelector('body').cloneNode(false);
+    after.style.color = 'blue';
 
-    const after1 = document.querySelector('body').cloneNode(false);
-    after1.style.color = 'blue';
-    g.add(after1, 'Body element after');
+    expect(g('Body element before', before, 'Body element after', after)).toMatchSnapshot('ELEMENT');
 
-    expect(g.values()).toMatchSnapshot('ELEMENT');
-
-    expect(group()
-        .add({}, 'Flag disabled')
-        .add({ enabled: true }, 'Flag enabled')
-        .values()
-    ).toMatchSnapshot('Object');
+    expect(g('Flag disabled', {}, 'Flag enabled', { enabled: true })).toMatchSnapshot('Object');
 });
